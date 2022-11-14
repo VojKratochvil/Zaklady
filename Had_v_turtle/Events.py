4b1872f39c0da6106368bf20a17c622eea89436c
VojtaK <vojcasek@seznam.cz>
2022-11-14 20:56:18 +0100

    hadí hra done

--- Had_v_turtle/Events.py
+++ Had_v_turtle/Events.py
@@ -1,16 +1,13 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-
 tommy = Turtle("turtle")
 
 def move_forward():
-    tommy.forward(10)
+    tommy.forward(20)
 
-
-# Button press
+# Stisknutí klávesy
 screen.listen()
 screen.onkeypress(move_forward, "w")
-
 
 screen.exitonclick()
--- Had_v_turtle/tracer.py
+++ Had_v_turtle/tracer.py
@@ -1,104 +1,26 @@
 from turtle import Turtle, Screen
 import time
-import random
-
-screen_width = 800
-screen_height = 800
 
 screen = Screen()
 screen.bgcolor("green")
 screen.title("Vítejte v Hadí hře")
-screen.setup(width=screen_width, height=screen_height)
+screen.setup(width=600, height=600)
 screen.tracer(False)
 
-# Hlava
-head = Turtle("square")
-head.color("black")
-head.speed(0)
-head.penup()
-head.goto(0, 0)
-head.direction = "stop"
-# tělo
-body_parts = []
+square1 = Turtle("square")
+square1.penup()
+square1.goto(0, 0)
+square2 = Turtle("square")
+square2.penup()
+square2.goto(-20, 0)
 
-# Jídlo pro hada
-food = Turtle("circle")
-food.color("red")
-food.penup()
-x_start = random.randint(-screen_width / 2 + 20, screen_width / 2 + 20)
-y_start = random.randint(-screen_height / 2 + 20, screen_height / 2 + 20)
-food.goto(x_start, y_start)
-
-def move():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y + 20)
-
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y - 20)
-
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
-
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
-
-
-# Fuknce : připnout k Events.py
-def move_up():
-    head.direction = "up"
-
-def move_down():
-    head.direction = "down"
-
-def move_left():
-    head.direction = "left"
-
-def move_right():
-    head.direction = "right"
-
-
-screen.listen()
-screen.onkeypress(move_up, "w")
-screen.onkeypress(move_down, "s")
-screen.onkeypress(move_left, "a")
-screen.onkeypress(move_right, "d")
-
-
-# Pohyb plynulý a hlavní cyklus
-while True:
+for _ in range(80):
+    square1.forward(10)
+    square2.forward(10)
+    time.sleep(0.1)
     screen.update()
 
-
-
-    move()
-    if head.distance(food) < 20:
-        x = random.randint(-screen_width/2 + 20, screen_width/2 + 20)
-        y = random.randint(-screen_height/2 + 20, screen_height/2 + 20)
-        food.goto(x, y)
-
-        # Hadí tělo
-        new_body_part = Turtle("square")
-        new_body_part.speed(0)
-        new_body_part.color("grey")
-        new_body_part.penup()
-        body_parts.append(new_body_part)
-
-    for index in range(len(body_parts) -1, 0, -1):
-        x = body_parts[index - 1].xcor()
-        y = body_parts[index - 1].ycor()
-        body_parts[index].goto(x, y)
-
-    if len(body_parts) > 0:
-        x = head.xcor()
-        y = head.ycor()
-        body_parts[0].goto(x, y)
-
-    move()
-    time.sleep(0.15)
+screen.exitonclick()
 
 
 
